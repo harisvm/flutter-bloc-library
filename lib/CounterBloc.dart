14a163fb6a96930aca41004ab541a76bf3038050
harry$ <harinarayananvm1692@gmail.com>
2020-05-23 11:39:55 +0530

    bloc tut

--- lib/CounterBloc.dart
+++ lib/CounterBloc.dart
@@ -1,22 +1,28 @@
-
-
- import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutterbloclibrary/counter_event.dart';
 import 'package:flutterbloclibrary/counter_state.dart';
 
-class CounterBloc extends Bloc<CounterEvent, CounterState>{
+class CounterBloc extends Bloc<CounterEvent, CounterState> {
   @override
   // TODO: implement initialState
-  CounterState get initialState => throw UnimplementedError();
+  CounterState get initialState => CounterState.initialState();
+  CounterState counterState;
 
   @override
-  Stream<CounterState> mapEventToState(CounterEvent event) {
-
-    // TODO: implement mapEventToState
-    throw UnimplementedError();
+  Stream<CounterState> mapEventToState(CounterEvent event, ) async* {
+    if (CounterEvent is IncrementEvent) {
+      print(counterState..counter);
+      yield counterState..counter += 1;
+    } else if (CounterEvent is EcrementEvent) {
+      yield counterState..counter -= 1;
+    }
   }
-  
-  
-  
-  
+
+  void dispose( ) {
+
+
+
+
+
+  }
 }
